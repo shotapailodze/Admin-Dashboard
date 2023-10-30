f01d1b75fe596623870481552b47a381e02230b1
shotapailodze <shotikophailodze1998@gmail.com>
2023-10-30 16:51:48 +0400

    menu design done

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import './Menu.scss'
+import { Link } from 'react-router-dom'
+import {menu} from '../../data'
 
 const Menu = () => {
   return (
     <div className="menu">
-        Menu
+      {menu.map(item => (
+      <div className="item" key={item.id}>
+          <span className="title">{item.title}</span>
+          {item.listItems.map(listItem => (
+            <Link className='listItem' to={listItem.url} key={listItem.id}>
+              <img src={listItem.icon} alt=''/>
+              <span className="listItemTitle">{listItem.title}</span>
+            </Link>
+            
+            ))}
+        </div>
+
+      ) )}
+        
     </div>
   )
 }
