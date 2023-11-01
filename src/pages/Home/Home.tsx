0eb0e92bdd19d5ce67fbdfe8e63cf27975515035
shotapailodze <shotikophailodze1998@gmail.com>
2023-11-01 16:32:12 +0400

    animated charts done

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,5 @@
 import BarchartBox from '../../components/BarchartBox/BarchartBox'
+import BigChartBox from '../../components/BigChartBox/BigChartBox'
 import Chartbox from '../../components/Chartbox/Chartbox'
 import PieChartBox from '../../components/PieChartBox/PieChartBox'
 import TopBox from '../../components/Topbox/TopBox'
@@ -16,7 +17,7 @@ const Home = () => {
         <div className="box box4"><PieChartBox /></div>
         <div className="box box5"><Chartbox {...chartBoxConversion}/></div>
         <div className="box box6"><Chartbox {...chartBoxRevenue} /></div>
-        <div className="box box7">box7</div>
+        <div className="box box7"><BigChartBox /></div>
         <div className="box box8"><BarchartBox {...barChartBoxVisit} /></div>
         <div className="box box9"><BarchartBox {...barChartBoxRevenue} /></div>
     </div>
