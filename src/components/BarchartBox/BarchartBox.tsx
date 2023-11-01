1f166ea1a188c72988a542f99217508f0c7070f8
shotapailodze <shotikophailodze1998@gmail.com>
2023-11-01 16:25:26 +0400

    animated chart

---
+++ src/components/BarchartBox/BarchartBox.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { BarChart, Bar, ResponsiveContainer,Tooltip } from 'recharts';
+import './BarchartBox.scss'
+
+
+type Props = {
+    title: string;
+    color: string;
+    dataKey: string;
+    chartData: object[];
+}
+
+const BarchartBox = (props: Props) => {
+  return (
+    <div className='barchartbox'>
+        <h1>{props.title}</h1>
+        <div className="chart">
+        <ResponsiveContainer width="99%" height={150}>
+            <BarChart data={props.chartData}>
+                <Tooltip
+                contentStyle={{backgroundColor: '#2a3447', borderRadius: '5px'}}
+                labelStyle={{display: 'none'}}
+                cursor={{fill: 'none'}}
+                />
+                <Bar dataKey={props.dataKey} fill={props.color} />
+            </BarChart>
+        </ResponsiveContainer>
+        </div>
+    </div>
+  )
+}
+
+export default BarchartBox
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,8 @@
+import BarchartBox from '../../components/BarchartBox/BarchartBox'
 import Chartbox from '../../components/Chartbox/Chartbox'
+import PieChartBox from '../../components/PieChartBox/PieChartBox'
 import TopBox from '../../components/Topbox/TopBox'
-import { chartBoxConversion, chartBoxProduct, chartBoxRevenue, chartBoxUser } from '../../data'
+import { barChartBoxRevenue, barChartBoxVisit, chartBoxConversion, chartBoxProduct, chartBoxRevenue, chartBoxUser } from '../../data'
 import './Home.scss'
 
 const Home = () => {
@@ -11,12 +13,12 @@ const Home = () => {
         </div>
         <div className="box box2"><Chartbox {...chartBoxUser} /></div>
         <div className="box box3"><Chartbox {...chartBoxProduct} /></div>
-        <div className="box box4"></div>
+        <div className="box box4"><PieChartBox /></div>
         <div className="box box5"><Chartbox {...chartBoxConversion}/></div>
         <div className="box box6"><Chartbox {...chartBoxRevenue} /></div>
         <div className="box box7">box7</div>
-        <div className="box box8">box8</div>
-        <div className="box box9">box9</div>
+        <div className="box box8"><BarchartBox {...barChartBoxVisit} /></div>
+        <div className="box box9"><BarchartBox {...barChartBoxRevenue} /></div>
     </div>
   )
 }
